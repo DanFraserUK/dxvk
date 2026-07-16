25987588bb65739deb8429753a2bfecaf225b6ef
DanFraser <11152006+DanFraserUK@users.noreply.github.com>
2026-07-16 23:11:10 +0100

    [SMP-DIAG-SANITY]

--- src/d3d11/d3d11_main.cpp
+++ src/d3d11/d3d11_main.cpp
@@ -105,21 +105,16 @@ extern "C" {
       return E_FAIL;
     }
   }
-  
-  
+
   static HRESULT D3D11InternalCreateDeviceAndSwapChain(
-          IDXGIAdapter*         pAdapter,
-          D3D_DRIVER_TYPE       DriverType,
-          HMODULE               Software,
-          UINT                  Flags,
-    const D3D_FEATURE_LEVEL*    pFeatureLevels,
-          UINT                  FeatureLevels,
-          UINT                  SDKVersion,
-    const DXGI_SWAP_CHAIN_DESC* pSwapChainDesc,
-          IDXGISwapChain**      ppSwapChain,
-          ID3D11Device**        ppDevice,
-          D3D_FEATURE_LEVEL*    pFeatureLevel,
-          ID3D11DeviceContext** ppImmediateContext) {
+      IDXGIAdapter *pAdapter, D3D_DRIVER_TYPE DriverType, HMODULE Software,
+      UINT Flags, const D3D_FEATURE_LEVEL *pFeatureLevels, UINT FeatureLevels,
+      UINT SDKVersion, const DXGI_SWAP_CHAIN_DESC *pSwapChainDesc,
+      IDXGISwapChain **ppSwapChain, ID3D11Device **ppDevice,
+      D3D_FEATURE_LEVEL *pFeatureLevel,
+      ID3D11DeviceContext **ppImmediateContext) {
+    std::cerr << "[SMP-DIAG-SANITY] D3D11InternalCreateDeviceAndSwapChain ENTER"
+              << std::endl;
     InitReturnPtr(ppDevice);
     InitReturnPtr(ppSwapChain);
     InitReturnPtr(ppImmediateContext);
@@ -216,7 +211,6 @@ extern "C" {
     
     return S_OK;
   }
-  
 
     DLLEXPORT HRESULT __stdcall D3D11CoreCreateDevice(
             IDXGIFactory*       pFactory,
